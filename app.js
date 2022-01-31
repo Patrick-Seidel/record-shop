db8e0ed779cb90a0f0f8f4afb52462edbabccf36
Patrick-Seidel <p.seidel.1987@gmail.com>
2022-01-31 10:29:47 +0100

    Task 01 - Mock database and Controllers

---
+++ app.js
@@ -0,0 +1,42 @@
+/** EXTERNAL DEPENDENCIES */
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+/** ROUTERS */
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recordsRouter = require('./routes/records');
+
+/** INIT */
+const app = express();
+
+/** LOGGING */
+app.use(logger('dev'));
+
+
+/** SETTING UP LOWDB */
+const adapter = new FileSync('data/db.json');
+const db = low(adapter);
+db.defaults({ records:[] }).write();
+
+
+/** REQUEST PARSERS */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/** STATIC FILES*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/** ROUTES */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/records', recordsRouter);
+
+/** EXPORT PATH */
+module.exports = app;
